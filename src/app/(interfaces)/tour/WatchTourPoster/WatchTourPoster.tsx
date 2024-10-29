fdfea4bda3233da17c3a3ac80dfce91ee4948d0d
Nicolas Dubois <nicolas_db@outlook.fr>
2024-10-29 11:12:34 +0100

    fix: update tour video thumbnail

--- src/app/(interfaces)/tour/WatchTourPoster/WatchTourPoster.tsx
+++ src/app/(interfaces)/tour/WatchTourPoster/WatchTourPoster.tsx
@@ -11,7 +11,7 @@ function WatchTourPoster() {
 			onClick={openModal}
 			className="mt-8 max-w-[960px] transition-all duration-[400ms] rounded-3xl overflow-hidden hover:shadow-[0_100px_140px_-50px_rgba(0,0,0,.25),0_0_20px_0_rgba(0,0,0,.03)] hover:scale-[1.01]"
 		>
-			<Image src="/assets/tour/video.jpg" alt="" width={2000} height={1126} />
+			<Image src="/assets/tour/Video.jpg" alt="" width={2000} height={1126} />
 		</button>
 	);
 }
